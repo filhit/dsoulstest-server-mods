356c0826b618ef900eb06a04550235c3f26fbf9a
filhit <filhit@gmail.com>
2019-08-08 11:22:24 +0000

    Initial commit

---
+++ init.lua
@@ -0,0 +1,8 @@
+local modpath = minetest.get_modpath("minetest_sounds")
+local soundsdir = modpath.."/sounds"
+local soundfiles = minetest.get_dir_list(soundsdir)
+minetest.log("minetest_sounds loaded")
+for _,file in pairs(soundfiles) do
+  minetest.log(file)
+end
+
