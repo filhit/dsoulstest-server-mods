f48b76a99055b13f23cf5f276f72d7abb81c43df
filhit <filhit@gmail.com>
2019-08-21 15:51:30 +0000

    Fix server_sounds rename

--- server_sounds/init.lua
+++ server_sounds/init.lua
@@ -1,4 +1,4 @@
-local modpath = minetest.get_modpath("minetest_sounds")
+local modpath = minetest.get_modpath("server_sounds")
 local soundsdir = modpath.."/sounds"
 local soundfiles = minetest.get_dir_list(soundsdir)
 minetest.log("minetest_sounds loaded")
