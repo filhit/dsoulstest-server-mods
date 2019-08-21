910255d21dc3d1a481c7b64122411f6e520e33d0
filhit <filhit@gmail.com>
2019-08-21 18:20:28 +0000

    Connect mobs_mc and bows mods

---
+++ mobs_mc_gameconfig/init.lua
@@ -0,0 +1,9 @@
+mobs_mc = {}
+mobs_mc.override = {}
+mobs_mc.override.items = {}
+mobs_mc.override.items.bow = "bows:bow_wood"
+mobs_mc.override.items.arrow = "bows:arrow"
+mobs_mc.override.enderman_block_texture_overrides = {}
+minetest.register_alias("mobs_mc:bow_wood", "bows:bow_wood")
+minetest.register_alias("mobs_mc:arrow", "bows:arrow")
+minetest.log("action", "[mobs_mc_gameconfig] loaded!")
