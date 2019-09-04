728bb8bcf5b7edf474f70ef4582dcce8ce2807f9
filhit <filhit@gmail.com>
2019-09-04 21:55:32 +0000

    Don't make an alias for bow.
    Bows don't respect aliases and the game crashes if such an aliased bow
    is fired.

--- mobs_mc_gameconfig/init.lua
+++ mobs_mc_gameconfig/init.lua
@@ -1,7 +1,7 @@
 mobs_mc = {}
 mobs_mc.override = {}
 mobs_mc.override.items = {}
-mobs_mc.override.items.bow = "bows:bow_wood"
+-- mobs_mc.override.items.bow = "bows:bow_wood"
 mobs_mc.override.items.arrow = "bows:arrow"
 mobs_mc.override.enderman_block_texture_overrides = {}
 minetest.register_alias("mobs_mc:bow_wood", "bows:bow_wood")
