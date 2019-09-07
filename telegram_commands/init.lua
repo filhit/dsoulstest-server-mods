f6b07b15d3dec66ad865c100ab304681c1c5fe20
filhit <filhit@gmail.com>
2019-09-07 13:52:29 +0000

    Add shutdown command

--- telegram_commands/init.lua
+++ telegram_commands/init.lua
@@ -1,13 +1,37 @@
+local ie = minetest.request_insecure_environment()
+local configurationMessage = "Mod telegram_commands requires insecure environment. Add secure.trusted_mods = telegram_commands to minetest.conf"
+assert(ie, configurationMessage)
+local shutdown_threshold = 10
+
+local function get_command_args(command, msg)
+  return string.sub(msg.text, string.len("\\" .. command .. " ") + 1)
+end
+
 local msg_command = "msg"
 telegram.register_command(msg_command, function(msg)     
-  minetest.chat_send_all(string.sub(msg.text, string.len("\\" .. msg_command .. " ")))
+  minetest.chat_send_all(get_command_args(msg_command, msg))
 end)
 
 local sound_command = "sound"
 telegram.register_command(sound_command, function(msg)
-  local sound_name = string.sub(msg.text, string.len("\\" .. sound_command .. " ") + 1)
+  local sound_name = get_command_args(sound_command, msg)
   minetest.log("action", "[telegram_commands] playing sound \"" .. sound_name .. "\"")
   minetest.sound_play(sound_name, { gain = 1.0 })
+end)
+
+local shutdown_command = "shutdown"
+telegram.register_command(shutdown_command, function(msg)
+  local seconds_ago = ie.os.time() - msg.date
+  if seconds_ago < shutdown_threshold then
+    minetest.log("action", "[telegram_commands] received command to shut down " .. seconds_ago .. " seconds ago. Shutting down.")
+    local message = get_command_args(shutdown_command, msg)
+    if message then
+      minetest.request_shutdown(message)
+    end
+    ie.os.execute("/usr/bin/sudo /sbin/shutdown now")
+  else
+    minetest.log("action", "[telegram_commands] received command to shut down " .. seconds_ago .. " seconds ago. The message is stale. Ignoring.")
+  end
 end)
 
 minetest.log("action", "[telegram_commands] loaded!")
